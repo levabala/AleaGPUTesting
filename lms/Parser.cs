0ddc464f8d548765812bb0249e219ee70a985c7a
Levabala <levabala@ya.ru>
2017-10-15 17:28:26 +0300

    v0.4

--- lms/Parser.cs
+++ lms/Parser.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace lms
@@ -10,6 +13,125 @@ namespace lms
     {
         public static void Parse(
             List<string> filesNames, int strob, int channelsCount, int channelWidth, 
-            int framesCount, )
+            int framesCount, int tau, double[] kt, int[] detectors, int channel0,
+			ParameterizedThreadStart SummatorCall)
+		{
+			int frame = 0;
+			int save_count = 0;
+			HashSet<int> detectorsHashSet = new HashSet<int>(detectors);			
+
+			for (int k = 0; k < filesNames.Count; k++)
+			{
+				string nam = filesNames[k];
+
+				FileStream fs = File.OpenRead(nam);
+				long len = fs.Length;
+
+				BinaryReader br = new BinaryReader(fs);
+				long pos = 0;
+
+				Stopwatch sw = Stopwatch.StartNew();
+
+				float speed_mbs = 0;
+				float speed_x = 0;
+				int lastFrameNeutrons = 0;
+
+				int[] kk = new int[0x100];
+
+				long fbeg = 0;
+				long fend = 0;
+				long f0 = 0;
+				long f1 = 0;
+				long f2 = 0;
+				long f3 = 0;
+				long f4 = 0;
+				long f5 = 0;
+				long f6 = 0;
+				long f7 = 0;
+				long f8 = 0;				
+
+				List<int>[] neutrons = new List<int>[detectors.Length];
+				for (int d = 0; d < neutrons.Length; d++)
+				{
+					neutrons[d] = new List<int>();
+				}
+
+				int neutronsCount = 0;
+
+				while (pos < len)
+				{
+					Stopwatch sw2 = Stopwatch.StartNew();
+
+					byte[] buf = br.ReadBytes(1000000);
+					pos += buf.Length;					
+
+					for (int i = 0; i < buf.Length; i++)
+					{
+						int lo = (buf[i++]) | (buf[i++] << 8) | (buf[i++] << 16);
+
+						byte hi = buf[i];
+						kk[hi]++;												
+
+						switch (hi)
+						{
+							case 0xf0: f0 = lo; break;
+							case 0xf1: f1 = lo; break;
+							case 0xf2: f2 = (long)lo | ((long)f4) << 24; break;
+							case 0xf3: f3 = (long)lo | ((long)f4) << 24; break;
+							case 0xf4: f4++; break;
+							case 0xf5: f5 = (long)lo | ((long)f4) << 24; break;
+							case 0xf6: f6 = (long)lo | ((long)f4) << 24; break;
+							case 0xf7: f7 = (long)lo | ((long)f4) << 24; break;
+							case 0xf8: f8 = (long)lo | ((long)f4) << 24; break;
+							case 0xfb: fbeg = (f0 | ((long)(f4) << 24)); break;
+							case 0xfa:
+								fend = (long)lo | ((long)(f4) << 24); frame += 1;
+								if (frame % framesCount == 0)
+								{
+									float spec_time = (float)(fend * 16e-9);									
+
+									int neutronsDelta = neutronsCount - lastFrameNeutrons;
+									lastFrameNeutrons = neutronsCount;
+
+									int[][] array = new int[neutrons.Length][];
+									for (int ii = 0; ii < array.Length; ii++)
+										array[ii] = neutrons[ii].ToArray();
+
+									Thread summatorThread = new Thread(SummatorCall);
+									summatorThread.IsBackground = true;
+									summatorThread.Start(array);
+
+									for (int d = 0; d < neutrons.Length; d++)									
+										neutrons[d] = new List<int>();									
+
+									speed_x = (float)(spec_time / sw.Elapsed.TotalSeconds);
+									speed_mbs = (float)(buf.Length / sw2.Elapsed.TotalSeconds / 1000000.0);
+
+									Console.WriteLine(
+										"save: {0,5}  time: {1,6:f2}  frame: {2,6}  speed: {3,6:f2}x  neutronsCount: {4}  neutronsDelta: {5}", 
+										save_count, spec_time, frame, speed_x, neutronsCount, neutronsDelta);
+								}
+								break;
+						}
+
+
+						if (hi < detectors.Length && detectorsHashSet.Contains(hi))//detectors.Contains(hi))
+						{
+							long t = (long)lo | ((long)f4) << 24;
+							if (t > fbeg && fbeg > fend)
+							{
+								float tmks = (float)((t - fbeg) * 16e-3);
+								int tch = (int)(tmks / tau * kt[hi]) - channel0;
+								if (tch >= 0 && tch < channelsCount)
+								{
+									neutrons[hi].Add(tch);
+									neutronsCount++;
+								}						
+							}
+						}
+					}
+				}
+			}
+		}
     }
 }
--- lms/Program.cs
+++ lms/Program.cs
@@ -6,93 +6,238 @@ using System.Threading.Tasks;
 using Alea;
 using Alea.Parallel;
 using System.Diagnostics;
+using System.IO;
+using System.Threading;
 
 namespace lms
 {
     class Program
-    {
-        static int length = 10000000;
-        static int rounds = 100;
-        static int chunks = 10;
+	{
+		#region variables
+		static int ref_ch0 = 0;
+		static int ref_chan = 31600;
+		static int ref_strob = 200;
+		static int strob = 20;
+		static int ref_det = 0;
+		static List<int> dets = new List<int>();
+		static double ref_phase = 0.7;
+		static double ref_k = 0.7128;
+		static double ref_r = 1 / ref_k;
+		static int ref_tau = 1;
+		static string ref_out = "low";
+		static int ref_frames = 50;
+		static List<string> names = new List<string>() { "*_raw.0??" };
+		static List<string> namelist = new List<string>();
+		static int max_det = 32;
+		static int max_mks = 32768;
+		static int maxch = max_mks / ref_tau; //32000;
+		static int ref_delay1 = 3200;
+		static bool save_all = false;
+		static bool save_sum = false;
+		static bool save_ssum = false;
 
-        static void Main(string[] args)
-        {
-            Random rnd = new Random();
-            double[] randomArr = new double[length];
-            for (int i = 0; i < length; i++)
-                randomArr[i] = Math.Round(rnd.NextDouble() * 10, 3);
+		static List<int[]> sp = new List<int[]>(max_det);
+		static double[] kt = new double[]
+		{
+			1,
+			1,
+			0.9963,
+			0.9968,
+			0.9942,
+			0.9944,
+			1,
+			1,
+			0.9970,
+			0.9976,
+			0.9944,
+			0.9946,
+			1,
+			1,
+			1,
+			1,
+			1,
+			1,
+			1,
+			1,
+			1,
+			1,
+			1,
+			1,
+			1,
+			1,
+			1,
+			1,
+			1,
+			1,
+			1,
+			1
+		};
+		#endregion		
 
-            Console.WriteLine("Elements count: {0}\nRounds count: {1}\n", length, rounds);
+		static Summator summator;
 
-            Stopwatch sw = new Stopwatch();
+		static void Main(string[] args)
+        {			
+			init(args);
 
-            sw.Start();
-            DeviceMemory<double> memory = alea(randomArr);
-            sw.Stop();
-            long gpuAllocating = sw.ElapsedMilliseconds;
-            Console.WriteLine("Device memory allocated: {0}", gpuAllocating);
-            sw.Reset();
+			ParameterizedThreadStart summatorThreadStart = new ParameterizedThreadStart(SummatorCall);
 
-            sw.Start();
-            for (int i = 0; i < rounds; i++)
-                alea2(memory);            
-            sw.Stop();
-            long gpuCalc = sw.ElapsedMilliseconds;
+			summator = new SummatorGPU(ref_chan, max_mks, dets.ToArray(), strob);
+			Parser.Parse(namelist, strob, ref_chan, max_mks, ref_frames, ref_tau, kt, dets.ToArray(), ref_ch0, summatorThreadStart);
 
-            var data = Gpu.CopyToHost(memory);
-            
-            sw.Start();
-            double[] array = new double[length];
-            for (int i = 0; i < array.Length; i++)
-                array[i] = randomArr[i];
-            sw.Stop();
-            long cpuAllocating = sw.ElapsedMilliseconds;
-            Console.WriteLine("Host memory allocated: {0}\n", cpuAllocating);
-            sw.Reset();
+			/*Visualizer visual = new Visualizer();			
+			visual.DrawSpec(summator.channels[0]);
+			visual.Show();*/
 
-            sw.Start();
-            for (int ii = 0; ii < rounds; ii++)
-                for (int i = 0; i < array.Length; i++)
-                    array[i] *= (Math.Sin(array[i]));
-            sw.Stop();
-            long cpuCalc = sw.ElapsedMilliseconds;
-
-            double[] differenceChunks = new double[chunks];
-            double totalDiff = 0;
-            Parallel.For(0, length, i =>
-            {
-                double diff = Math.Abs(array[i] - data[i]);
-                int chunkIndex = i / (length / chunks);
-                differenceChunks[chunkIndex] += diff;
-                totalDiff += diff;
-            });
-
-            Console.WriteLine("Gpu elapsed: {0}\nCpu elapsed: {1}\n", gpuCalc, cpuCalc);
-            Console.WriteLine("Difference: {0}", totalDiff);
-            Console.WriteLine("Average diff: {0}", totalDiff / length);
-            Console.WriteLine("Average diff per round: {0}", totalDiff / length / rounds);
-            for (int i = 0; i < chunks; i++)
-                Console.WriteLine("DifferenceChunk{0}: {1}", i, differenceChunks[i]);
             Console.ReadKey();
         }
 
-        static DeviceMemory<double> alea(double[] array)
-        {
-            DeviceMemory<double> memory = Gpu.Default.AllocateDevice<double>(length);
-            deviceptr<double> pointer = memory.Ptr;            
-            Gpu.Default.For(0, length, i => pointer[i] = array[i]);
+		public static void SummatorCall(object arg)
+		{
+			int[][] neutrons = arg as int[][];
+			summator.AddValues(neutrons);			
+		}
 
-            //var data = Gpu.CopyToHost(memory);
+		public static void init(string[] args)
+		{
+			string help_msg =
+			"List Mode Transformer by db' 2017-10-14\n\n" +
+			"usage: lmt <params> \n\n" +
+			"use single param as <filename> or <search_mask> for .raw files in current folder \n" +
+			"like <zns6_raw.01> or <*_raw.???> \n\n" +
+			"combination of following -keys can be used:\n\n" +
+			"-raw <mask or name> - listmode files to load\n" +
+			"-det <flag> - detector flag to calc (0x00-0xEF)\n" +
+			"-dor - select detectors 0-11\n" +
+			"-dpr - select detectors 12-31\n" +
+			"-strob <mks> - strob width (800)\n" +
+				//"-delay1 <mks> - start-signal 0xf0 to reactor-pulse delay (3300)\n" +
+				//"-delay2 <mks> - first channel of -h file (1700)\n" +
+			"-ch0 <number> - first channel to write (0) \n" +
+			"-chan <number> - number of channels to write (7900)\n" +
+				//"-tau <mks> - time-channel width of -h file (2)\n" +
+				//"-h <filename> - load .h reference spectrum (blue)\n" +
+				//"-cfg <filename> - load .config from file\n" +
+			"-w <mks> - calculate with channel width tau = <mks> \n" +
+			"-o <name> - output folder\n" +
+			"-frames <number> - N frames to sum/write in low res mode\n" +
+			"-all - save all calculated detectors\n" +
+			"-sum - save only sum calculated\n" +
+			"-ssum - save super sum\n"
+			;
 
-            return memory;
-        }
+			//string[] args = Environment.GetCommandLineArgs();
+			if (args.Length < 1)
+			{
+				Console.WriteLine(help_msg);
+				return;
+			}
 
-        static void alea2(DeviceMemory<double> memory)
-        {
-            deviceptr<double> pointer = memory.Ptr;
-            Gpu.Default.For(0, length, i => pointer[i] *= (DeviceFunction.Sin(pointer[i])));
+			names.Clear();
 
-            //var data = Gpu.CopyToHost(memory);
-        }
+			if (args.Length == 1) // one argument treating as single .raw or search mask
+			{
+				names.Add(args[1]);
+			}
+
+			if (args.Length > 1)
+			{
+				for (int i = 0; i < args.Length; i++)
+				{
+					switch (args[i])
+					{
+						case "-raw": names.Add(args[i + 1]); break;
+						case "-det": if (int.TryParse(args[i + 1], out ref_det)) dets.Add(ref_det); break;
+						case "-mks": int.TryParse(args[i + 1], out max_mks); break;
+						//case "-hkl": ReadHKL(args[i + 1]); break;
+						//case "-tau": int.TryParse(args[i + 1], out ref_tau); break;
+						case "-delay1": int.TryParse(args[i + 1], out ref_delay1); break;
+						//case "-delay2": if (int.TryParse(args[i + 1], out ref_delay2)) Delay2Mks = ref_delay2; break;
+						//case "-chan": if (int.TryParse(args[i + 1], out ref_chan))
+						//		SetupMaxMks(ref_chan); break;
+						//case "-ch0": if (int.TryParse(args[i + 1], out ref_ch0))
+						//		SetupMaxMks(ref_chan); break;
+						case "-strob": int.TryParse(args[i + 1], out ref_strob); break;
+						case "-phase": double.TryParse(args[i + 1], out ref_phase); break;
+						case "-r": if (double.TryParse(args[i + 1], out ref_r)) ref_k = 1 / ref_r; break;
+						case "-k": if (double.TryParse(args[i + 1], out ref_k)) ref_r = 1 / ref_k; break;
+						//case "-h": LoadH(ref_hname = args[i + 1]); break;
+						//case "-cfg": config_path = args[i + 1]; break;
+						//case "-run": run = true; break;
+						//case "-pos": if (int.TryParse(args[i + 1], out ref_pos)) c.invert = (ref_pos == 1 ? true : false); break;
+						//case "-deb": c.debounce = true; break;
+						//case "-w": int.TryParse(args[i + 1], out ref_tau); SetupMaxChan(max_mks); break;
+						case "-o": ref_out = args[i + 1]; break;
+						//case "-anal": anal = true; break;
+						//case "-avg": avg = true; break;
+						//case "-rpm": write_rpm = true; break;
+						//case "-nolog": c.nolog = true; break;
+						//case "-ff": if (double.TryParse(args[i + 1], out ref_ff)) c.ff = ref_ff; break;
+						case "-frames": int.TryParse(args[i + 1], out ref_frames); break;
+						//case "-low": c.lowRes = true; break;
+						case "-dor": SelectDOR(); break;
+						case "-dpr": SelectDPR(); break;
+						case "-all": save_all = true; break;
+						case "-sum": save_sum = true; break;
+						case "-ssum": save_ssum = true; break;
+					}
+				}
+			}
+
+			maxch = max_mks / ref_tau;
+			for (int d = 0; d < max_det; d++) sp.Add(new int[maxch]);
+			strob = ref_strob / ref_tau;			
+
+			namelist = new List<string>();
+
+			foreach (string s in names)
+			{
+				if (s.Contains('*') || s.Contains('?'))
+				{
+					string[] ss = Directory.GetFiles(".", s);
+					if (ss.Length > 0) namelist.AddRange(ss);
+				}
+				else
+					if (File.Exists(s)) namelist.Add(s);
+			}
+			namelist.Sort();
+
+			foreach (string fn in namelist)
+			{
+				Console.WriteLine(fn);
+			}
+
+			foreach (int d in dets)
+			{
+				Console.Write("{0} ", d);
+			}
+			Console.WriteLine();
+			Console.WriteLine("w = {0} mks", ref_tau);
+			Console.WriteLine("strob = {0} chan", ref_strob);
+			Console.WriteLine("frames = {0}", ref_frames);
+			Console.WriteLine("output = {0}", ref_out);
+
+			//			Console.ReadKey();
+
+			if (!Directory.Exists(ref_out)) Directory.CreateDirectory(ref_out);
+		}
+
+		static void SelectDOR()
+		{
+			dets.AddRange(Enumerable.Range(0, 12));
+		}
+		static void SelectDPR()
+		{
+			dets.AddRange(Enumerable.Range(12, 20));
+		}
+
+		static void RemoveDuplets(List<int> l)
+		{
+			l.Sort();
+			for (int i = 1; i < l.Count; i++)
+			{
+				if (l[i] == l[i - 1]) l.RemoveAt(i);
+			}
+		}
     }
 }
