0d357b5f52ead90a06d48ec7787ef97455e447d8
Levabala <levabala@ya.ru>
2017-10-15 17:09:09 +0300

    v0.2

--- lms/Summator.cs
+++ lms/Summator.cs
@@ -7,19 +7,22 @@ using System.Threading.Tasks;
 namespace lms
 {
     abstract class Summator
-    {
-        public double[][] channels; //detector - channel_value
-        public int channelsCount, channelWidth, strob;
-        public int[] detectors;
-        public Summator(int channelsCount, int channelWidth, int[] detectors, int strob)
+    {		
+		public static double[][] channels; //detector - channel_value
+		public static int channelsCount, channelWidth, strob;
+		public static int[] detectors;
+        public Summator(int chcount, int chwidth, int[] dets, int strob_value)
         {
-            this.channelsCount = channelsCount;
-            this.channelWidth = channelWidth;
-            this.detectors = detectors;
-            this.strob = strob;
+			channelsCount = chcount;
+			channelWidth = chwidth;
+			detectors = dets;
+			strob = strob_value;
+			channels = new double[dets.Length][];
+			for (int i = 0; i < channels.Length; i++)
+				channels[i] = new double[channelsCount];
         }
 
-        virtual public void AddValues(int[][] neutrons) //detector - neutron_channel
+		virtual public void AddValues(int[][] neutrons) //detector - neutron_channel
         {
 
         }                
